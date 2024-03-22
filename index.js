7f43c92fb84b299cf5cc24830adf3e7a1b4b5e70
Vitor Mateus <vitor7jan@gmail.com>
2024-03-21 22:08:12 -0300

    selecionando site pelo contador

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
-  console.log(req.query);
-  res.send();
+const getSiteUrl = async (counter) => {
+  const reponse = await fetch(process.env.FINANCIAL_CONTENT_URLS);
+  const json = await reponse.json();
+  const siteList = json.urls;
+  let siteNumber = counter % siteList.length;
+  siteNumber = siteNumber <= 0 ? siteList.length : siteNumber;
+  const siteUrl = siteList[siteNumber - 1];
+  return siteUrl;
+};
+
+app.get("/financial-content", async (req, res) => {
+  try {
+    const siteUrl = await getSiteUrl(req.query.counter);
+    res.redirect(siteUrl);
+  } catch (error) {
+    console.log(error);
+    res.send();
+  }
 });
 
 app.listen(8000, () => console.log("api started"));
