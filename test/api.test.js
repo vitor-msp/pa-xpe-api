a715f958fc080f92cd226ad4a01c6221c16c72e0
Vitor Mateus <vitor7jan@gmail.com>
2024-03-28 10:37:24 -0300

    adicionados testes na api

--- test/api.test.js
+++ test/api.test.js
@@ -27,4 +27,41 @@ describe("api tests", () => {
 
   beforeAll(buildTest);
 
+  test("should return blacklist with status code 200", async () => {
+    const response = await supertest(api).get("/api/blacklist").send();
+    expect(response.statusCode).toBe(200);
+    expect(response.body.urls).toEqual(blacklist);
+  });
+
+  test("should redirect to YOUTUBE url with status code 302", async () => {
+    const response = await supertest(api)
+      .get("/api/financial-content?counter=1")
+      .send();
+    expect(response.statusCode).toBe(302);
+    expect(response.headers["location"]).toEqual("https://www.youtube.com/");
+  });
+
+  test("should redirect to GOOGLE url with status code 302", async () => {
+    const response = await supertest(api)
+      .get("/api/financial-content?counter=2")
+      .send();
+    expect(response.statusCode).toBe(302);
+    expect(response.headers["location"]).toEqual("https://www.google.com/");
+  });
+
+  test("should redirect to UOL url with status code 302", async () => {
+    const response = await supertest(api)
+      .get("/api/financial-content?counter=3")
+      .send();
+    expect(response.statusCode).toBe(302);
+    expect(response.headers["location"]).toEqual("https://www.uol.com.br/");
+  });
+
+  test("should redirect to YOUTUBE url with status code 302 when counter not informed", async () => {
+    const response = await supertest(api)
+      .get("/api/financial-content")
+      .send();
+    expect(response.statusCode).toBe(302);
+    expect(response.headers["location"]).toEqual("https://www.youtube.com/");
+  });
 });
